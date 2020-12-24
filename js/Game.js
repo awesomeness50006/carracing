d4f024d055d47a5b959d7990e0cd6d9a02e0f37f
awesomeness50006 <72236097+awesomeness50006@users.noreply.github.com>
2020-12-23 20:04:04 -0600

    Add files via upload

--- js/Game.js
+++ js/Game.js
@@ -1,7 +1,5 @@
 class Game {
-  constructor(){
-
-  }
+  constructor(){}
 
   getState(){
     var gameStateRef  = database.ref('gameState');
@@ -28,57 +26,61 @@ class Game {
       form = new Form()
       form.display();
     }
-
     car1 = createSprite(100,200);
+    car1.addImage(car1I)
     car2 = createSprite(300,200);
+    car2.addImage(car2I)
     car3 = createSprite(500,200);
+    car3.addImage(car3I)
     car4 = createSprite(700,200);
-    cars = [car1, car2, car3, car4];
+    car4.addImage(car4I)
+    cars = [car1, car2, car3, car4]
   }
 
   play(){
     form.hide();
-
+    textSize(30);
+    text("Game Start", 120, 100)
     Player.getPlayerInfo();
-    
+
     if(allPlayers !== undefined){
-      //var display_position = 100;
-      
-      //index of the array
+      background(198,135,103)
+      image(trackI, 0, -displayHeight*4, displayWidth, displayHeight*5)
       var index = 0;
-
-      //x and y position of the cars
-      var x = 0;
-      var y;
-
+      var x = 280;
+      var y ;
       for(var plr in allPlayers){
-        //add 1 to the index for every loop
-        index = index + 1 ;
-
-        //position the cars a little away from each other in x direction
-        x = x + 200;
-        //use data form the database to display the cars in y direction
-        y = displayHeight - allPlayers[plr].distance;
-        cars[index-1].x = x;
+        index = index+1;
+        x= x+250;
+        y = displayHeight-allPlayers[plr].distance;
+        cars[index-1].x =x;
         cars[index-1].y = y;
-
-        if (index === player.index){
-          cars[index - 1].shapeColor = "red";
-          camera.position.x = displayWidth/2;
-          camera.position.y = cars[index-1].y
-        }
+        if (index == player.index){
+          stroke(10)
+          fill("black")
+          rect(x-32.5, y+50, 65, 65)
+     cars[index-1].shapeColor = "red";
+     camera.position.x = displayWidth/2;
+     camera.position.y = cars[index-1].y
        
-        //textSize(15);
-        //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
       }
-
     }
+    
+    }
+ 
 
     if(keyIsDown(UP_ARROW) && player.index !== null){
-      player.distance +=10
+      player.distance +=50
       player.update();
     }
-
+    if (player.distance>5200){
+      gameState = 2;
+    }
     drawSprites();
+  }
+  end(){
+console.log("Game Ended")
+
+
   }
 }
--- sketch.js
+++ sketch.js
@@ -7,12 +7,24 @@ var distance = 0;
 var database;
 
 var form, player, game;
+var car1, car2, car3, car4,cars;
+var car1I, car2I, car3I, car4I
+var groundI
+var trackI
+function preload(){
+car1I = loadImage("images/car1.png")
+car2I = loadImage("images/car2.png")
+car3I = loadImage("images/car3.png")
+car4I = loadImage("images/car4.png")
+groundI = loadImage("images/ground.png")
+trackI = loadImage("images/track.jpg")
+}
 
-var cars, car1, car2, car3, car4;
+
 
 
 function setup(){
-  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  canvas = createCanvas(displayWidth-20,displayHeight-30);
   database = firebase.database();
   game = new Game();
   game.getState();
@@ -27,5 +39,9 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState===2){
+    game.end();
+
   }
 }
